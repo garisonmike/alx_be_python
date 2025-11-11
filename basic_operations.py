ec68823a0482a4dd535dfabbe383e0563699f04e
Aleckqhie <reckalecks@gmail.com>
2025-11-11 22:17:50 +0300

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10 
+number2 = 5 
+# addition of number1 and number2 
+sum = number1+ number2
+# subraction of number1 and number2 
+subtraction = number1 - number2 
+# multiplication of number1 and number2 
+product = number1 * number2
+#output the results 
+print (f"Addition of {number1} and {number2} is {sum}")
+print (f"subtracion of {number1} and {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {product}")
+
+
+
---
+++ future_age_calculator.py
@@ -0,0 +1,3 @@
+age =int (input("how old are you ?"))
+new_age = age +23
+print (f"in 2050 you will be {new_age} years old ")
